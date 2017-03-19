a2d8697bc7e69fe46f3a8ce1dc505a1dd7031083
Matthew Powers <mtpowers@gmail.com>
2017-03-19 18:06:13 -0400

    adding in the react reducer and store

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -3,4 +3,22 @@ import React from 'react';
 import { render } from 'react-dom';
 import css from './styles/style.styl';
 
-render(<p>hi</p>,document.getElementById('root'));
+//Import Components
+import Main from './components/Main';
+import Single from './components/Single';
+import Photogrid from './components/Photogrid';
+
+//Import react router deps
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+
+const router = (
+    <Router history={browserHistory}>
+        <Route path="/" component={Main}>
+            <IndexRoute component={Photogrid}></IndexRoute>
+            <Route path="/view/:postId" component={Single}></Route>
+        </Route>
+    </Router>
+)
+
+render(router,document.getElementById('root'));
